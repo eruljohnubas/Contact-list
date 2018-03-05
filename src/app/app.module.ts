53409543bd89b0c13d2c98b4897339dd9f9e8f5d
Erul <erulggubas@yahoo.com>
2018-03-06 07:10:54 +0800

    modified css,html,component,module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { FormsModule } from '@angular/forms';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyC9-qh97lIneDYbqbA8XCn1-XT4rLDh3Mc",
+  authDomain: "contactlist-b3174.firebaseapp.com",
+  databaseURL: "https://contactlist-b3174.firebaseio.com",
+  projectId: "contactlist-b3174",
+  storageBucket: "contactlist-b3174.appspot.com",
+  messagingSenderId: "188726472244"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig), 
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
